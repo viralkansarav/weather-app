2f2430b46c0397b7abd5d9fde4ca42f0690c5ab1
viralkansarav <viralkansarav@gmail.com>
2023-09-09 22:10:01 +0530

    add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WeatherProject from './WeatherProject';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('roots'));
 root.render(
 
     <WeatherProject />
