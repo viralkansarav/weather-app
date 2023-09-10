0517a50d7c84cf9f2d1cef3a1c7a359a466bef66
viralkansarav <viralkansarav@gmail.com>
2023-09-10 11:48:17 +0530

    deployed website

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WeatherProject from './WeatherProject';
 
-const root = ReactDOM.createRoot(document.getElementById('roots'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <WeatherProject />
- 
 );
 
